40f3194d023f0999a985adb0c0e5273a4a7cce69
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-03-24 17:36:05 +0200

    preparing files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+console.log(process.env.API_KEY);
+
+// module.exports = {
+//   // ...
+//   plugins: [
+//     // ...
+//     somePlugin({
+//       apiKey: process.env.API_KEY,
+//     }),
+//   ],
+// };
