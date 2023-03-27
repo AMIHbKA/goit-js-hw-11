7c4d3f0528510055cbf486f9ece6993f9eb9ab6e
AMIHbKA <bishop0013forum@gmail.com>
2023-03-27 05:55:41 +0300

    added some code and logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,69 @@
-require('dotenv').config();
+import ImageSearch from './js/get_images';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-console.log(process.env.API_KEY);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.search-form'),
+};
 
-// module.exports = {
-//   // ...
-//   plugins: [
-//     // ...
-//     somePlugin({
-//       apiKey: process.env.API_KEY,
-//     }),
-//   ],
-// };
+function renderGalleryImages(hits) {
+  let gallery = hits.map(
+    ({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `
+    <div class="photo-card">
+      <a class="link__photo-card" >
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+        <div class="info">
+            <p class="info-item">
+                <b>Likes: </b>${formattedNumber(likes)}
+            </p>
+            <p class="info-item">
+                <b>Views: </b>${formattedNumber(views)}
+            </p>
+            <p class="info-item">
+                <b>Comments: </b>${formattedNumber(comments)}
+            </p>
+            <p class="info-item">
+                <b>Downloads: </b>${formattedNumber(downloads)}
+            </p>
+        </div>
+    </div>
+`;
+    }
+  );
+
+  refs.gallery.innerHTML = gallery.join('');
+}
+
+const newSearchImages = new ImageSearch(process.env.API_KEY);
+
+document.addEventListener('submit', onSearch);
+
+async function onSearch(e) {
+  e.preventDefault();
+  newSearchImages.query = e.srcElement.searchQuery.value;
+
+  if (newSearchImages.query === '') {
+    return;
+  }
+
+  newSearchImages.resetPage();
+  clearGalleryContainer();
+
+  try {
+    const gallery = await newSearchImages.searchImages();
+
+    renderGalleryImages(gallery);
+  } catch (error) {
+    Notify.failure(error.message);
+  }
+}
+
+function formattedNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
+function clearGalleryContainer() {
+  refs.gallery.innerHTML = '';
+}
---
+++ src/js/get_images.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+class ImageSearch {
+  constructor(apiKey) {
+    this.API_URL = 'https://pixabay.com/api/';
+    this.API_KEY = apiKey;
+    this.perPage = 40;
+    this.page = 1;
+    this.maxPage = 0;
+    this.totalHits = 0;
+    this.searchQuery = '';
+  }
+
+  async searchImages() {
+    const options = {
+      params: {
+        key: this.API_KEY,
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: this.perPage,
+        page: this.page,
+      },
+    };
+
+    try {
+      const response = await axios.get(this.API_URL, options);
+      const { hits, totalHits } = response.data;
+
+      this.totalHits = totalHits;
+      this.maxPage = Math.ceil(this.totalHits / this.perPage);
+
+      if (this.maxPage === this.page) {
+        //   console.log('pages', this.maxPage, this.page, this.totalHits);
+        //   Если изображений нет, тогда срабатывает ошибка ниже, так как ТоталХится имеет всего 1 запись
+        throw new Error(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+
+      this.incrementPage();
+      return hits;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  incrementPage() {
+    if (this.page >= this.maxPage) {
+      this.page = this.maxPage;
+    } else {
+      this.page += 1;
+    }
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
+export default ImageSearch;
